94517005c0777a45a5407666658e16f77a6a2ce5
kacollins <kimberlyacollins@gmail.com>
2015-12-02 10:47:54 -0600

    Added connection string in connections.config file.

--- TablePopulation/Program.cs
+++ TablePopulation/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,47 @@ namespace TablePopulation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter table name:");
+            string tableName = Console.ReadLine();
+
+            string queryText = $"SELECT * FROM {tableName}";
+            DataTable dt = GetDataTable(queryText);
+            Console.WriteLine(dt);
+
+            Console.WriteLine("Press enter to exit:");
+            Console.Read();
         }
+
+        #region Methods
+
+        private static DataTable GetDataTable(string queryText)
+        {
+            SqlConnection conn = new SqlConnection
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings["SQLDBConnection"].ToString()
+            };
+
+            SqlDataAdapter sda = new SqlDataAdapter(queryText, conn);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                conn.Open();
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        
+        #endregion
+        
     }
 }
